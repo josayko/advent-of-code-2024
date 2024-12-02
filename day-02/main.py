a34f2964afdba4a1d3a59b3f3c586480600f3a90
Jonny Saykosy <josayko@pm.me>
2024-12-02 22:24:08 +0100

    bug: part 2

--- day-02/main.py
+++ day-02/main.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def is_decreasing(line: List[int]) -> bool:
+def is_decreasing(line: List[int]):
     tmp = line[0]
     for n in line[1:]:
         diff = tmp - n
@@ -11,7 +11,7 @@ def is_decreasing(line: List[int]) -> bool:
     return True
 
 
-def is_increasing(line: List[int]) -> bool:
+def is_increasing(line: List[int]):
     tmp = line[0]
     for n in line[1:]:
         diff = n - tmp
@@ -31,18 +31,51 @@ def check_safety(line: List[int]) -> bool:
     return False
 
 
+def check_safety_with_skip(line: List[int]) -> bool:
+    # decrease
+    if line[1] < line[0]:
+        is_safe = is_decreasing(line)
+        if not is_safe:
+            for j in range(len(line)):
+                slice = line[:j] + line[j + 1 :]
+                if check_safety(slice):
+                    print(slice)
+                    return True
+        return is_safe
+    # increase
+    elif line[1] > line[0]:
+        is_safe = is_increasing(line)
+        if not is_safe:
+            count = 0
+            for j in range(len(line)):
+                slice = line[:j] + line[j + 1 :]
+                count += 1
+                if check_safety(slice):
+                    print(slice)
+                    return True
+        return is_safe
+    return False
+
+
 def main():
     with open("day-02/input.txt") as f:
         lines = f.readlines()
 
     safe_count = 0
+    safe_count_with_skip = 0
     for line in lines:
         line = line.strip().split()
         line = [int(n) for n in line]
-        if check_safety(line):
-            safe_count += 1
+        # if check_safety(line):
+        #     safe_count += 1
+        if check_safety_with_skip(line):
+            print("[OK]", line)
+            safe_count_with_skip += 1
+        else:
+            print("[NOT OK]", line)
 
     print(f"Numbers of safe reports: {safe_count}")
+    print(f"Numbers of safe reports with skip: {safe_count_with_skip}")
     return
 
 
