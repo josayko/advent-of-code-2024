fe9a582cd5e9d229d6364904c669a192630e5843
Jonny Saykosy <josayko@pm.me>
2024-12-02 22:24:08 +0100

    feat: day-02 part two done

--- day-02/main.py
+++ day-02/main.py
@@ -2,33 +2,33 @@ from typing import List
 
 
 def is_decreasing(line: List[int]) -> bool:
-    tmp = line[0]
-    for n in line[1:]:
-        diff = tmp - n
+    for i in range(len(line) - 1):
+        diff = line[i] - line[i + 1]
         if diff < 1 or diff > 3:
             return False
-        tmp = n
     return True
 
 
 def is_increasing(line: List[int]) -> bool:
-    tmp = line[0]
-    for n in line[1:]:
-        diff = n - tmp
+    for i in range(len(line) - 1):
+        diff = line[i + 1] - line[i]
         if diff < 1 or diff > 3:
             return False
-        tmp = n
     return True
 
 
 def check_safety(line: List[int]) -> bool:
-    # decrease
-    if line[1] < line[0]:
-        return is_decreasing(line)
-    # increase
-    elif line[1] > line[0]:
-        return is_increasing(line)
-    return False
+    return is_increasing(line) or is_decreasing(line)
+
+
+def check_safety_with_skip(line: List[int]) -> bool:
+    is_safe = check_safety(line)
+    if not is_safe:
+        for j in range(len(line)):
+            slice = line[:j] + line[j + 1 :]
+            if check_safety(slice):
+                return True
+    return is_safe
 
 
 def main():
@@ -36,13 +36,16 @@ def main():
         lines = f.readlines()
 
     safe_count = 0
+    safe_count_with_skip = 0
     for line in lines:
-        line = line.strip().split()
-        line = [int(n) for n in line]
+        line = [int(n) for n in line.split()]
         if check_safety(line):
             safe_count += 1
+        if check_safety_with_skip(line):
+            safe_count_with_skip += 1
 
     print(f"Numbers of safe reports: {safe_count}")
+    print(f"Numbers of safe reports with skip: {safe_count_with_skip}")
     return
 
 
